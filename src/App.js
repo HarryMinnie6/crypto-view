251e38ca13099f6147aeffef11611505c823e149
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2021-04-28 18:34:09 +0200

    finished basic api

--- src/App.js
+++ src/App.js
@@ -23,66 +23,86 @@ function App() {
         headers: {
           "x-rapidapi-key":
             "491ccbcd4dmshd157ea3d7e474efp152ba1jsn60cc5fb9a981",
-          "x-rapidapi-host": "coinranking1.p.rapidapi.com"
-        }
+          "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+        },
       })
         .then((response) => response.json())
         .then((data) => {
           let retrievedCoins = data.data.coins;
-          console.log("data ", data);
-          console.log("coins", coins);
+
           setCoins(retrievedCoins);
-          console.log(retrievedCoins[0]);
+          var delayInMilliseconds = 2000; //1 second
+
+          setTimeout(function () {
+            console.log("coins", coins);
+          }, delayInMilliseconds);
         });
     };
 
     getAllCoinsData();
   }, []);
 
+  const requestOptions = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "491ccbcd4dmshd157ea3d7e474efp152ba1jsn60cc5fb9a981",
+      "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+    },
+  };
+
   const onCoinChange = async (event) => {
     const coinCode = event.target.value;
-    console.log("coin cde", coinCode);
+    console.log("coin code", coinCode);
     // setting state for the country...
 
     const url =
-      coinCode === "coin"
+      coinCode === coin.id
         ? "https://coinranking1.p.rapidapi.com/coins"
-        : `https://coinranking1.p.rapidapi.com/coin/${coinCode}`;
+        : `https://coinranking1.p.rapidapi.com/coin/${coinCode} `;
 
-    await fetch(url)
+    await fetch(url, requestOptions)
       .then((response) => response.json())
       .then((data) => {
         setCoin(coinCode);
         setCoinInfo(data);
 
-        // console.log("coinInfo", coinInfo);
+        // console.log("coinInfo from url", coinInfo);
       });
 
     console.log("coin info >> ", coinInfo);
   };
 
   return (
-    <div className="app">
+    <div className='app'>
       <h1>test</h1>
+
       <FormControl>
         <Select
-          className="header__selectBox"
-          variant="outlined"
+          className='header__selectBox'
+          variant='outlined'
           value={coin}
           onChange={onCoinChange}
         >
           {/* All Coins option */}
-          <MenuItem value="coin">coin</MenuItem>
+          <MenuItem value='coin'>coin</MenuItem>
 
           {/* Renders all the coins */}
           {coins.map((coin) => (
-            <MenuItem value={coin.id}> {coin.name}</MenuItem>
+            <MenuItem key={coin.name} value={coin.id}>
+              {" "}
+              {coin.name}
+            </MenuItem>
           ))}
         </Select>
       </FormControl>
-      {/* {coins.map((coin) => (
-        <p>{coin.name}</p>
-      ))} */}
+
+      {coins.map((coin) => (
+        <div>
+          <p>{coin.name}</p>
+          <p>{coin.symbol}</p>
+          <p>{coin.iconUrl}</p>
+        </div>
+      ))}
     </div>
   );
 }
